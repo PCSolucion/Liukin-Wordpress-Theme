95c3ef0bb22c8b767cb146365f05640d5ae95b99
Jose Manuel Moreno <contacto@pc-solucion.es>
2020-12-04 04:57:45 +0100

    Sidebar Integrated

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 sidebar">
-        <?php if ( is_active_sidebar( 'widgets-derecha' ) ) : ?>
-        <?php dynamic_sidebar( 'widgets-derecha' ); ?>
-        <?php else : ?>
-        <?php endif; ?>
+<?php if ( is_active_sidebar( 'widgets-derecha' ) ) : ?>
+<?php dynamic_sidebar( 'widgets-derecha' ); ?>
+<?php else : ?>
+<?php endif; ?>
 </div>
