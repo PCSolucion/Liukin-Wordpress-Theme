f54ec3103dd674bc457d4911e220850ecce112c2
José Manuel Moreno <36554682+PCSolucion@users.noreply.github.com>
2020-12-24 05:40:04 +0100

    Use DIV for search button

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
     ?>
 	  <nav class="navbar navbar-expand-md navbar-light bg-white">
       <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <div class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </div>
         <div class="navbar-brand">
 			    <?php 
             the_custom_logo();
